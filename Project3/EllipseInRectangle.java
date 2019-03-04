e1323bbaaac1b582974926f66a587fd043ce68f2
KingCornWallis <m.darabaris@att.net>
2019-03-04 17:42:09 -0600

    Add files via upload

---
+++ Project3/EllipseInRectangle.java
@@ -0,0 +1,41 @@
+//Jacob Darabaris
+//2/20/18
+//CS 290
+
+
+package CH.ifa.draw.figures;
+import java.awt.*;
+
+public class EllipselnRectangleFigure extends EllipseFigure
+{
+	public EllipselnRectangleFigure()
+	{
+		super(new Point(0,0), new Point(0,0));
+	}
+
+	public EllipselnRectangleFigure(Point origin, Point corner)
+	{
+		super(origin, corner);
+	}
+
+	//Use override because parent classes have the same name
+	@Override public void drawBackground(Graphics g) 
+	{ 
+		Rectangle r = displayBox();
+		super.drawBackground(g); //use parent method 
+		g.fillRect(r.x, r.y, r.width, r.height);
+	}
+
+	@Override public void drawFrame(Graphics g) 
+	{ 
+		Rectangle r = displayBox();
+		g.drawOval(r.x, r.y, r.width-1, r.height-1); // Draws the Ellipse
+		g.drawRect(r.x, r.y, r.width, r.height); //Draws the rectangle
+	}
+	
+
+	// CODE FROM JavaDrawApp.java
+	// tool= new UndoableTool(new CreationTool(this, new EllipselnRectangleFigure()));
+	// palette.add(createToolButton(IMAGES + "RECT", "EclipselnRectangle Tool",	tool));
+
+}
---
+++ Project3/README.txt
@@ -0,0 +1,10 @@
+// This repository contains project 3 for CS 290 for Spring 2018.
+
+The purpose of this project is to create a custom figure to use in JHotDraw.
+
+This is a relatively simple project; the code provided creates a "Rectangle with an Ellipse inside of it" object.
+
+The entire project has too many supporting files to be included on Github, only the parts
+I programmed are included.
+
+This project is implemented through Java.
